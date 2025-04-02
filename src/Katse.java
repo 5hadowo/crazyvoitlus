2b35b97cc8a42bfeedf04db9afc20b80f1bc084f
neletais <nele.taisi@gmail.com>
2025-04-02 22:47:10 +0300

    Just in case

---
+++ src/Katse.java
@@ -0,0 +1,8 @@
+public class Katse {
+    public static void main(String[] args) {
+        int rünnak = 5;
+        int upgrade = 3;
+        int number = (int) (Math.random()*rünnak+1);
+        System.out.println(number);
+    }
+}
--- src/Mängija.java
+++ src/Mängija.java
@@ -11,17 +11,50 @@ public class Mängija {
         this.kilp = kilp;
     }
 
-    public void ründa(){
+    public int ründa(){
         //tagastab ründe tugevuse (arvestades algset tugevust ja kasutades Math.random)
+        //upgrade -> "baas" rünnak suureneb
+        return suvalineNumber(rünnak);
     }
 
-    public void ravimine(){
+    public int ravimine(){
         //lisab eludele random numbri elusid
+        return suvalineNumber(elud);
     }
 
-    public void kaitse(){
+    public int kaitse(){
         //lisab "punkte" kilbile
+        return suvalineNumber(kilp);
     }
 
+    public int suvalineNumber(int tegevus){
+        //genereerib suvalise numbri
+        int number = (int) (Math.random()*tegevus+1); //0->tegevus
+        return number;
 
+    }
+
+    public int getElud() {
+        return elud;
+    }
+
+    public void setElud(int elud) {
+        this.elud = elud;
+    }
+
+    public int getKilp() {
+        return kilp;
+    }
+
+    public void setKilp(int kilp) {
+        this.kilp = kilp;
+    }
+
+    public int getRünnak() {
+        return rünnak;
+    }
+
+    public void setRünnak(int rünnak) {
+        this.rünnak = rünnak;
+    }
 }
